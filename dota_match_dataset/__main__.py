13f70fdd546d1df5b54bed1cd7cb559feb82cc84
Zeynep Bicer <mericzeynepbicer@gmail.com>
2021-01-02 16:49:35 +0000

    initial commit

--- dota_match_dataset/__main__.py
+++ dota_match_dataset/__main__.py
@@ -9,8 +9,8 @@ __all__ = ["main"]
 def main():
     start_time = time.time()
     fetch_dota_dataset_concurrent(open_dota_api=OpenDotaApi(os.getenv("OPEN_DOTA_API_KEY")),
-                                  minimum_number_of_matches=2, game_mode=22, lobby_types=[5, 6, 7],
-                                  minimum_match_duration=900, maximum_number_of_matches=5,
+                                  minimum_number_of_matches=1000, game_mode=22, lobby_types=[5, 6, 7],
+                                  minimum_match_duration=900, maximum_number_of_matches=1100,
                                   path_to_output_file="/tmp/dota-dataset.json")
     print(f'Finished in {time.time() - start_time} seconds')
 
